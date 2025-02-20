819b16412ff1c6e549e2c1a1dbab02be6e9c4e22
KsushaTarasova <ksutaras543@mail.ru>
2025-02-20 22:55:32 +0300

    is palindrome

---
+++ src/problems/is_palindrome/Main.java
@@ -0,0 +1,8 @@
+package problems.is_palindrome;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.isPalindrome(1213121));
+    }
+}
